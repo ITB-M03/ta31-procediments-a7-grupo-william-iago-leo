7ff523f057272174cf261e14a7b270bcc87c4729
IagoMacBook <iagomzzh@gmail.com>
2024-12-10 12:22:03 +0100

    ejercicio2.kt por arreglar

---
+++ src/main/kotlin/controllers/ejercicio2.kt
@@ -0,0 +1,75 @@
+package controllers
+
+import java.text.DateFormat
+import java.util.Arrays
+import java.util.Date
+import java.util.Scanner
+import javax.swing.text.DateFormatter
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+
+data class Impuestos(
+    val fecha:LocalDate,
+    val general:Int,
+    val reduit:Int,
+    val superReduit:Int,
+    val exempt:Int
+
+)
+
+
+data class datos(
+    val precio:Int,
+    val tipoIVa: Int,
+    val fechaFormateada:LocalDate
+)
+fun main(){
+    val scan = Scanner(System.`in`)
+    var almacenar =  pedirAlUsuario(scan)
+
+    fp
+}
+
+fun calcularIva(preu: Int, tipoIVa: Int, fechaCompra: LocalDate) {
+    val impuestos = listOf(
+        Impuestos(LocalDate.parse("01-01-1986", DateTimeFormatter.ofPattern("dd-MM-yyyy")), 12, 6, 0, 0),
+        Impuestos(LocalDate.parse("01-01-1992", DateTimeFormatter.ofPattern("dd-MM-yyyy")), 15, 6, 0, 0),
+        Impuestos(LocalDate.parse("01-01-1993", DateTimeFormatter.ofPattern("dd-MM-yyyy")), 15, 6, 3, 0),
+        Impuestos(LocalDate.parse("01-01-1995", DateTimeFormatter.ofPattern("dd-MM-yyyy")), 16, 7, 4, 0),
+        Impuestos(LocalDate.parse("01-01-2010", DateTimeFormatter.ofPattern("dd-MM-yyyy")), 18, 8, 4, 0),
+        Impuestos(LocalDate.parse("15-07-2012", DateTimeFormatter.ofPattern("dd-MM-yyyy")), 21, 10, 4, 0)
+    )
+
+
+}
+
+fun pedirAlUsuario(scanner: Scanner): List<datos> {
+    println("Dime el precio de lo que has comprado")
+    val precio = scanner.nextInt()
+    println("""Dime el tipo de iva que tiene el producto (Indicamelo con el numero de cada opción)
+        1 -- general
+        2 -- Reduit
+        3 -- Superreduit
+        4 -- Exempt
+    """.trimIndent())
+    val tipoIva = scanner.nextInt()
+    println("Dime el día de la compra")
+    val fechaCompra_dia = scanner.nextInt()
+    println("Dime el mes de la compra")
+    val fechaCompra_mes = scanner.nextInt()
+    println("Dime el año de la compra")
+    val fechaCompra_año = scanner.nextInt()
+
+    // Definir el formato
+    val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+
+    // Crear la fecha usando LocalDate
+    val fechaCompra = LocalDate.of(fechaCompra_año, fechaCompra_mes, fechaCompra_dia)
+    val fechaFormateada = fechaCompra
+
+    println("Fecha de compra formateada: $fechaFormateada")
+
+    val lista: List<datos> = listOf(datos(precio, tipoIva, fechaFormateada))
+    return lista
+}
